f55db876aaef695e1014240973418f9dc6b6fe22
CCRami <tabib.rami@esprit.tn>
2024-05-04 14:16:37 +0100

    fixed merging Agence into main

--- src/main/java/org/example/MainFx.java
+++ src/main/java/org/example/MainFx.java
@@ -7,6 +7,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class MainFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
